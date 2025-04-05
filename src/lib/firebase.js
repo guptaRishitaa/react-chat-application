829262413c115a96b3015cae7c8bb02e8672936f
guptaRishitaa <rishitagupta2700@gmail.com>
2025-04-04 20:08:22 -0400

    updared firebase with secrets

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,25 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import {getAuth} from "firebase/auth";
-// import {getFirestore} from "firebase/firestore";
-// import {getStorage} from "firebase/storage";
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA8tlQMlqcWv9xQtVSFmi2rKYcCwBke82Q",
-//   authDomain: "reactchat-90d8c.firebaseapp.com",
-//   projectId: "reactchat-90d8c",
-//   storageBucket: "reactchat-90d8c.appspot.com",
-//   messagingSenderId: "681139172235",
-//   appId: "1:681139172235:web:170ea44cc13455e40ab58e"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const auth = getAuth()
-// export const db = getFirestore()
-// export const storage = getStorage()
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -27,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCko6tXkMD6ojheBowAV1Gi2Zx55M5ra0Y",
-  authDomain: "chatify-6734b.firebaseapp.com",
-  projectId: "chatify-6734b",
-  storageBucket: "chatify-6734b.firebasestorage.app",
-  messagingSenderId: "775312493827",
-  appId: "1:775312493827:web:f51400cf3ffd84a38ced36",
-  measurementId: "G-01GQMQ9XZM"
+  apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
